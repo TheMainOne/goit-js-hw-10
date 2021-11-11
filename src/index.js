804d08aca7bbbc75cf4c52748334f16901173be3
Maksym Lvov <leva880@ukr.net>
2021-11-11 15:22:44 +0300

    some manipulations

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { fetchCountries } from './fetchCountries';
 
 var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
-
 const refs = {
   input: document.querySelector('#search-box'),
   list: document.querySelector('.country-list'),
@@ -11,26 +10,32 @@ const refs = {
 };
 
 refs.input.addEventListener('input', debounce(() => {
-  const valueOfInput = refs.input.value;
+  const valueOfInput = refs.input.value.trim();
   console.log(valueOfInput);
+
+  if (valueOfInput === '') {
+    refs.list.innerHTML = '';
+    return;
+  }
+
   fetchCountries(valueOfInput)
     .then(countries => renderUserList(countries))
     .catch(error => console.log(error));
 },DEBOUNCE_DELAY));
 
+// Если пользователь полностью очищает поле поиска, то HTTP-запрос не выполняется, а разметка списка стран или информации о стране пропадает.
+
 
 function renderUserList(countries) {
   const markup = countries
     .map((country) => {
-      return `<li>
+      return `<li class="country-list__item">
           <p><b>Country</b>: ${country.name.official}</p>
           <p><b>Capital</b>: ${country.capital}</p>
           <p><b>Population</b>: ${country.population}</p>
         </li>`;
     })
       .join("");
-    console.log(markup);
-    // refs.list.innerHTML = markup;
+    refs.list.innerHTML = markup;
 }
 
-// console.log(refs.input);
