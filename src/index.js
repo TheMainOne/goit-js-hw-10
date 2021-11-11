c692f214544c77d99e3863f6e8150c47026b9161
Maksym Lvov <leva880@ukr.net>
2021-11-11 11:04:57 +0300

    added dependencies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    list: document.querySelector('country-list'),
+    container: document.querySelector('country-list'),
+}
+
+console.log(refs.input);
