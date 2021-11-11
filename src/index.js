19cfa3e32e2499d2c607edebcbfb02eaba6db6f8
Maksym Lvov <leva880@ukr.net>
2021-11-11 11:38:25 +0300

    markup added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-
 const refs = {
-    input: document.querySelector('#search-box'),
-    list: document.querySelector('country-list'),
-    container: document.querySelector('country-list'),
-}
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('country-list'),
+  container: document.querySelector('country-list'),
+};
 
-console.log(refs.input);
+refs.input.addEventListener('input', () => {
+  const valueOfInput = refs.input.value;
+  console.log(valueOfInput);
+  fetchCountries(valueOfInput)
+    .then(users => renderUserList(users))
+    .catch(error => console.log(error));
+});
+
+console.log(fetch('https://restcountries.com/v3.1/all'));
+
+// fetchUsersBtn.addEventListener("click", () => {
+//   fetchUsers()
+//     .then((users) => renderUserList(users))
+//     .catch((error) => console.log(error));
+// });
+
+// function renderUserList(users) {
+//   const markup = users
+//     .map((user) => {
+//       return `<li>
+//           <p><b>Name</b>: ${user.name}</p>
+//           <p><b>Email</b>: ${user.email}</p>
+//           <p><b>Company</b>: ${user.company.name}</p>
+//         </li>`;
+//     })
+//     .join("");
+//   userList.innerHTML = markup;
+// }
+
+// console.log(refs.input);
