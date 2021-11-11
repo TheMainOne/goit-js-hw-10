1afa61fbd6c1bbd01346974be472843689089af2
TheMainOne <83756212+TheMainOne@users.noreply.github.com>
2021-11-11 09:39:25 +0000

    Deploying to gh-pages from @ TheMainOne/goit-js-hw-10@19cfa3e32e2499d2c607edebcbfb02eaba6db6f8 🚀

--- src.8abfbb39.js
+++ src.8abfbb39.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"krre":[function(require,module,exports) {
 
+},{}],"SvwL":[function(require,module,exports) {
+"use strict";function e(e){return fetch(`\nhttps://restcountries.com/v3.1/name/${e}`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()})}Object.defineProperty(exports,"__esModule",{value:!0}),exports.fetchCountries=e;
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./css/styles.css");const e=300,t={input:document.querySelector("#search-box"),list:document.querySelector("country-list"),container:document.querySelector("country-list")};console.log(t.input);
-},{"./css/styles.css":"krre"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10/src.6b0519b7.js.map
+"use strict";require("./css/styles.css");var e=require("./fetchCountries");const t=300,c={input:document.querySelector("#search-box"),list:document.querySelector("country-list"),container:document.querySelector("country-list")};c.input.addEventListener("input",()=>{const t=c.input.value;console.log(t),(0,e.fetchCountries)(t).then(e=>renderUserList(e)).catch(e=>console.log(e))}),console.log(fetch("https://restcountries.com/v3.1/all"));
+},{"./css/styles.css":"krre","./fetchCountries":"SvwL"}]},{},["Focm"], null)
+//# sourceMappingURL=/goit-js-hw-10/src.8abfbb39.js.map
