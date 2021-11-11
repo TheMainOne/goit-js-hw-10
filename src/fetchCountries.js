6d6ef0309a689c19f5e68b05a451c7d672575290
Maksym Lvov <leva880@ukr.net>
2021-11-11 12:17:26 +0300

    added debounce method in source code

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,7 @@ export { fetchCountries };
 
 function fetchCountries(name) {
     return fetch(`
-https://restcountries.com/v3.1/name/${name}`).then(response => {
+https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(response => {
         if (!response.ok) {
             throw new Error(response.status);
         }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
@@ -9,26 +10,27 @@ const refs = {
   container: document.querySelector('country-list'),
 };
 
-refs.input.addEventListener('input', () => {
+refs.input.addEventListener('input', debounce(() => {
   const valueOfInput = refs.input.value;
   console.log(valueOfInput);
   fetchCountries(valueOfInput)
-    .then(users => renderUserList(users))
+    .then(countries => renderUserList(countries))
     .catch(error => console.log(error));
-});
+},DEBOUNCE_DELAY));
 
 
-function renderUserList(users) {
-  const markup = users
-    .map((user) => {
+function renderUserList(countries) {
+  const markup = countries
+    .map((country) => {
       return `<li>
-          <p><b>Name</b>: ${user.name}</p>
-          <p><b>Email</b>: ${user.email}</p>
-          <p><b>Company</b>: ${user.company.name}</p>
+          <p><b>Country</b>: ${country.name.official}</p>
+          <p><b>Capital</b>: ${country.capital}</p>
+          <p><b>Population</b>: ${country.population}</p>
         </li>`;
     })
-    .join("");
-  userList.innerHTML = markup;
+      .join("");
+    console.log(markup);
+    // refs.list.innerHTML = markup;
 }
 
 // console.log(refs.input);
