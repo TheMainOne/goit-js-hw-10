8ce92efa95f4ee052cc6ccaefec63dfdcb67e291
Maksym Lvov <leva880@ukr.net>
2021-11-12 12:05:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,50 +10,50 @@ const refs = {
   container: document.querySelector('.country-info'),
 };
 
-refs.input.addEventListener('input', debounce(() => {
-  const valueOfInput = refs.input.value.trim();
+refs.input.addEventListener(
+  'input',
+  debounce(() => {
+    const valueOfInput = refs.input.value.trim();
 
-  if (valueOfInput === '') {
-    refs.list.innerHTML = '';
-    refs.container.innerHTML = '';
-    return;
-  }
-
-  fetchCountries(valueOfInput)
-    .then(countries => {
-
-      if (countries.length > 10) {
-        Notify.info('Too many matches found. Please enter a more specific name.');
-        return;
-      } else if (countries.length >= 2 && countries.length <= 10) {
-        refs.container.innerHTML = '';
-        renderFewCountryList(countries);
-        return;
-      } else {
+    if (valueOfInput === '') {
       refs.list.innerHTML = '';
-        renderOneCountryList(countries);
-        
-      }
-    })
-    .catch(() => Notify.failure('Oops, there is no country with that name'));
-},DEBOUNCE_DELAY));
+      refs.container.innerHTML = '';
+      return;
+    }
 
+    fetchCountries(valueOfInput)
+      .then(countries => {
+        if (countries.length > 10) {
+          Notify.info('Too many matches found. Please enter a more specific name.');
+          return;
+        } else if (countries.length >= 2 && countries.length <= 10) {
+          refs.container.innerHTML = '';
+          renderFewCountryList(countries);
+          return;
+        } else {
+          refs.list.innerHTML = '';
+          renderOneCountryList(countries);
+        }
+      })
+      .catch(() => Notify.failure('Oops, there is no country with that name'));
+  }, DEBOUNCE_DELAY),
+);
 
 function renderFewCountryList(countries) {
   const markup = countries
-    .map((country) => {
+    .map(country => {
       return `<li class="country-list__item list">
           <img class='country-list__img' src="${country.flags.svg}" alt="flag of Country" width="30px">
           <p>${country.name.common}</p>
         </li>`;
     })
-      .join("");
-    refs.list.innerHTML = markup;
+    .join('');
+  refs.list.innerHTML = markup;
 }
 
 function renderOneCountryList(countries) {
   const markup = countries
-    .map((country) => {
+    .map(country => {
       const countryValues = Object.values(country);
       const languagesValues = Object.values(countryValues[3]);
 
@@ -68,7 +68,6 @@ function renderOneCountryList(countries) {
           <p><b>Languages</b>: ${languagesValues}</p>
         </li>`;
     })
-      .join("");
-    refs.container.innerHTML = markup;
+    .join('');
+  refs.container.innerHTML = markup;
 }
-
